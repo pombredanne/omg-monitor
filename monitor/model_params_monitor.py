4bbe58e82db5a8ab36dae5cd4fe1257339165f67
allanino <allaninocencio@yahoo.com.br>
2014-05-21 17:21:22 +0000

    Modified to use swarm based parameters (resolution set by hand).

--- monitor/model_params_monitor.py
+++ monitor/model_params_monitor.py
@@ -1,94 +1,86 @@
-MODEL_PARAMS = {
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
 
-    'aggregationInfo': 
-        {   'hours': 0, 
-            'microseconds': 0, 
-            'seconds': 0, 
-            'fields': [], 
-            'weeks': 0, 
-            'months': 0, 
-            'minutes': 0, 
-            'days': 0, 
-            'milliseconds': 0, 
-            'years': 0}, 
-
-    'model': 'CLA', 
-    'version': 1, 
-    'predictAheadTime': None, 
-
-    'modelParams': 
-        {   'sensorParams': {
-                'verbosity': 0, 
-                'encoders': {
-                    u'responsetime': {
-                        'fieldname': u'responsetime',
-                        'resolution': 50.0,
-                        'seed': 1,
-                        'name': u'responsetime',
-                        'type': 'RandomDistributedScalarEncoder'},
-                    # 'timestamp_dayOfWeek': {   'dayOfWeek': (21, 1),
-                    #                        'fieldname': u'time',
-                    #                        'name': u'timestamp_dayOfWeek',
-                    #                        'type': 'DateEncoder'},
-                    # 'timestamp_timeOfDay': {   'fieldname': u'time',
-                    #                        'name': u'timestamp_timeOfDay',
-                    #                        'timeOfDay': (21, 1),
-                    #                        'type': 'DateEncoder'},
-                    # 'timestamp_weekend': {   'fieldname': u'time',
-                    #                      'name': u'timestamp_weekend',
-                    #                      'type': 'DateEncoder',
-                    #                      'weekend': 21}
-                        }, 
-                'sensorAutoReset': None}, 
-
-            'anomalyParams': {
-                u'anomalyCacheRecords': None, 
-                u'autoDetectThreshold': None, 
-                u'autoDetectWaitRecords': None}, 
-
-            'spParams': {
-                'columnCount': 2048, 
-                'spVerbosity': 0, 
-                'randomSP': 1, 
-                'synPermConnected': 0.1, 
-                'numActivePerInhArea': 40, 
-                'seed': 1956, 
-                'globalInhibition': 1, 
-                'inputWidth': 0, 
-                'synPermInactiveDec': 0.01, 
-                'synPermActiveInc': 0.1, 
-                'coincInputPoolPct': 0.5}, 
-
-            'spEnable': True, 
-
-            'clParams': {
-                'alpha': 0.01, 
-                'clVerbosity': 0, 
-                'steps': '1', 
-                'regionName': 'CLAClassifierRegion'}, 
-
-            'inferenceType': 'TemporalAnomaly', 
-            'tpEnable': True, 
-            'tpParams': {
-                'columnCount': 2048, 
-                'activationThreshold': 16, 
-                'pamLength': 2, 
-                'cellsPerColumn': 32, 
-                'permanenceInc': 0.1, 
-                'minThreshold': 12, 
-                'verbosity': 0, 
-                'maxSynapsesPerSegment': 32, 
-                'outputType': 'normal', 
-                'initialPerm': 0.21, 
-                'globalDecay': 0.0, 
-                'maxAge': 0, 
-                'permanenceDec': 0.1, 
-                'seed': 1960, 
-                'newSynapseCount': 20, 
-                'maxSegmentsPerCell': 128, 
-                'temporalImp': 'cpp', 
-                'inputWidth': 2048}, 
-
-            'trainSPNetOnlyIfRequested': False
-            }
-        }
+MODEL_PARAMS = {'aggregationInfo': {'days': 0,
+                     'fields': [],
+                     'hours': 0,
+                     'microseconds': 0,
+                     'milliseconds': 0,
+                     'minutes': 0,
+                     'months': 0,
+                     'seconds': 0,
+                     'weeks': 0,
+                     'years': 0},
+ 'model': 'CLA',
+ 'modelParams': {'anomalyParams': {u'anomalyCacheRecords': None,
+                                   u'autoDetectThreshold': None,
+                                   u'autoDetectWaitRecords': None},
+                 'clParams': {'alpha': 0.0047013540243624075,
+                              'clVerbosity': 0,
+                              'regionName': 'CLAClassifierRegion',
+                              'steps': '1'},
+                 'inferenceType': 'TemporalAnomaly',
+                 'sensorParams': {'encoders': {u'responsetime': {'fieldname': 'responsetime',
+                                                                 'name': 'responsetime',
+                                                                 'resolution': 150,
+                                                                 'seed': 1,
+                                                                 'type': 'RandomDistributedScalarEncoder'},
+                                               u'status': None,
+                                               u'time_dayOfWeek': None,
+                                               u'time_timeOfDay': None,
+                                               u'time_weekend': None},
+                                  'sensorAutoReset': None,
+                                  'verbosity': 0},
+                 'spEnable': True,
+                 'spParams': {'columnCount': 2048,
+                              'globalInhibition': 1,
+                              'inputWidth': 0,
+                              'maxBoost': 2.0,
+                              'numActiveColumnsPerInhArea': 40,
+                              'potentialPct': 0.8,
+                              'seed': 1956,
+                              'spVerbosity': 0,
+                              'spatialImp': 'cpp',
+                              'synPermActiveInc': 0.05,
+                              'synPermConnected': 0.1,
+                              'synPermInactiveDec': 0.09669797994536977},
+                 'tpEnable': True,
+                 'tpParams': {'activationThreshold': 12,
+                              'cellsPerColumn': 32,
+                              'columnCount': 2048,
+                              'globalDecay': 0.0,
+                              'initialPerm': 0.21,
+                              'inputWidth': 2048,
+                              'maxAge': 0,
+                              'maxSegmentsPerCell': 128,
+                              'maxSynapsesPerSegment': 32,
+                              'minThreshold': 9,
+                              'newSynapseCount': 20,
+                              'outputType': 'normal',
+                              'pamLength': 1,
+                              'permanenceDec': 0.1,
+                              'permanenceInc': 0.1,
+                              'seed': 1960,
+                              'temporalImp': 'cpp',
+                              'verbosity': 0},
+                 'trainSPNetOnlyIfRequested': False},
+ 'predictAheadTime': None,
+ 'version': 1}

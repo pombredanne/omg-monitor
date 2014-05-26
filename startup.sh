185b0280ce79482a1059e5ca4910a2b79db55c61
allanino <allaninocencio@yahoo.com.br>
2014-05-26 11:03:33 -0300

    Use .log for log files.

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start redis server
-redis-server > server/public/log/redis &
+redis-server > server/public/log/redis.log &
 
 # Start monitors running NuPIC
-./start.py $* 2> server/public/log/processes &
+./start.py $* 2> server/public/log/processes.log &
 
 # Start Go server
 cd server
-./server > public/log/martini
+./server > public/log/martini.log

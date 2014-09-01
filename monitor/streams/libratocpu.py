ae798d57bf059389ad20da60e558cc2dcd776349
allanino <allaninocencio@yahoo.com.br>
2014-09-01 12:47:02 -0300

    Added Librato AWS CPU stream.

---
+++ monitor/streams/libratocpu.py
@@ -0,0 +1,112 @@
+import librato
+from datetime import datetime
+from collections import deque
+from base import BaseStream
+import logging
+import os
+import time
+
+logger = logging.getLogger(__name__)
+
+class LibratocpuStream(BaseStream):
+    """ Class to provide a stream of data to NuPIC. """
+    
+    @property
+    def value_label(self):
+        return "CPU utilization"
+    
+    @property
+    def value_unit(self):
+        return "%"
+
+    def __init__(self, config):
+        
+        super(LibratocpuStream, self).__init__(config)
+
+        # Set Pingdom object
+        self.libr = librato.connect(config['credentials']['username'], 
+                                       config['credentials']['token'])
+
+        # Default value to associate with timeouts (to have something to feed NuPIC)
+        self.timeout_default = 30000
+
+        # Setup logging
+        self.logger =  logger or logging.getLogger(__name__)
+        handler = logging.handlers.RotatingFileHandler(os.environ['LOG_DIR']+"/stream_%s.log" % self.name,
+                                                       maxBytes=1024*1024,
+                                                       backupCount=4,
+                                                      )
+
+        handler.setFormatter(logging.Formatter('[%(levelname)s/%(processName)s][%(asctime)s] %(name)s %(message)s'))
+        handler.setLevel(logging.INFO)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+
+    def historic_data(self):
+        """ Return a batch of data to be used at training """
+
+        time_now = int(time.time())
+        time_start = time_now - 60*60*24*3
+        historic_data = []
+        while time_start < time_now:
+            try:
+                cpu = self.libr.get('AWS.EC2.CPUUtilization', start_time=time_start, count=100, resolution=60, source=self.id)
+                measurements = cpu.measurements[self.id]
+            except Exception:
+                logger.warn("Could not get Librato AWS CPU results.", exc_info=True)
+                continue
+                 
+            for modelInput in measurements:
+                servertime  = modelInput['measure_time']
+                modelInput['time'] = datetime.utcfromtimestamp(servertime)
+
+                self.history.appendleft(int(modelInput['value']))
+                modelInput['value'] = self._moving_average()
+
+                historic_data.append(modelInput)
+            time_start = time_start + 100*60
+
+        return historic_data
+
+    def new_data(self):
+        """ Return list of new data points since last fetching. """
+
+        new_data = []
+        try:
+            librato_results = self.libr.get('AWS.EC2.CPUUtilization', count=5, resolution=60, source=self.id)
+        except Exception:
+            self.logger.warn("Could not get Librato AWS CPU results.", exc_info=True)
+            return new_data
+
+        # If any result contains new responses (ahead of [servetime]) process it. 
+        # We check the last 5 results, so that we don't many lose data points.
+        for modelInput in [librato_results[4], librato_results[3], librato_results[2], librato_results[1], librato_results[0]]:
+            if self.servertime < modelInput['measure_time']:
+                servertime  = modelInput['measure_time']
+                modelInput['time'] = datetime.utcfromtimestamp(servertime)
+
+                self.history.appendleft(int(modelInput['value']))
+                modelInput['value'] = self._moving_average()
+
+                new_data.append(modelInput)
+
+        return new_data
+
+    @classmethod
+    def available_streams(cls, credentials):
+        """ Return a list with available streams for the class implementing this. Should return a list : 
+                [{'value': v1, 'time': t1}, {'value': v2, 'time': t2}] 
+        """
+
+        # Get CPU measutements (we use it get the ID of the instances)
+        libr = librato.connect(credentials['username'], credentials['token'])
+       
+        cpu = libr.get('AWS.EC2.CPUUtilization', count=100, resolution=1)
+        instances_list = [i for i in cpu.measurements]
+
+        result = []
+        for id_ in instances_list:
+            result.append({'id': id_, 'name': id_}) # Doesn't have a way to get better names
+
+        return result
